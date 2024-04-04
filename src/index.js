570558f395557d64beea4d8f790999d8685a20e0
ruz145 <wanyonyi_andrew@yahoo.com>
2024-04-04 10:55:37 +0300

    new commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 const init = () => {
-  
-}
+
+// Targeting the DOM element "form"
+const inputForm = document.querySelector("form");
+
+// Adding event listeners
+inputForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.querySelector("input#searchByID");
+
+    console.log(input.value);
+
+fetch(`http://localhost:3000/movies/${input.value}`)
+  .then((response) => response.json())
+  .then((data) => {
+    const title = document.querySelector("section#movieDetails h4");
+    const summary = document.querySelector("section#movieDetails p");
+
+    title.innerText = data.title;
+    summary.innerText = data.summary;
+  });
+     });
+};
 
 document.addEventListener('DOMContentLoaded', init);
+
+
+
+
+    
